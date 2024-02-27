9ff704ee9b3a42cd715aad87b463a0b66e81863e
assadujjaman112 <assadujjamanmurad120@gmail.com>
2024-02-27 17:37:55 +0600

    added main section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { TfiWorld } from "react-icons/tfi";
 const Footer = () => {
   return (
     <section className="bg-red-600 pt-5 md:pt-8 pb-8 md:pb-10 lg:pb-12">
-      <div className="w-11/12 md:w-4/5 mx-auto  flex justify-between">
+      <div className="w-11/12 md:w-4/5 mx-auto  flex flex-col gap-5 lg:gap-0  pl-10 md:pl-24 lg:pl-0 lg:flex-row justify-between">
         <div className="flex items-center gap-3">
           <p className="bg-white rounded-full px-1 py-1">
             <HiPhone className="text-red-600 rounded-full "></HiPhone>
