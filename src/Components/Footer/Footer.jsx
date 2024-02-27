d67c5f4ddf1ee9ae8305ab403dff7446eda5989a
assadujjaman112 <assadujjamanmurad120@gmail.com>
2024-02-27 17:27:35 +0600

    added banner

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,28 @@
 import { HiPhone } from "react-icons/hi";
+import { FaFacebookF } from "react-icons/fa";
+import { TfiWorld } from "react-icons/tfi";
 
 const Footer = () => {
   return (
     <section className="bg-red-600 pt-5 md:pt-8 pb-8 md:pb-10 lg:pb-12">
       <div className="w-11/12 md:w-4/5 mx-auto  flex justify-between">
         <div className="flex items-center gap-3">
-          <HiPhone className="text-red-600 rounded-full bg-white"></HiPhone>
+          <p className="bg-white rounded-full px-1 py-1">
+            <HiPhone className="text-red-600 rounded-full "></HiPhone>
+          </p>
           <h1 className="text-white">Toll Free 1800 200 1234</h1>
         </div>
         <div className="flex items-center gap-3">
-          <HiPhone className="text-red-600 rounded-full bg-white"></HiPhone>
-          <h1 className="text-white">Toll Free 1800 200 1234</h1>
+          <p className="bg-white rounded-full px-1 py-1">
+            <FaFacebookF className="text-red-600 rounded-full "></FaFacebookF>
+          </p>
+          <h1 className="text-white">www.facebook.com/cripumps</h1>
         </div>
         <div className="flex items-center gap-3">
-          <HiPhone className="text-red-600 rounded-full bg-white"></HiPhone>
-          <h1 className="text-white">Toll Free 1800 200 1234</h1>
+          <p className="">
+            <TfiWorld className="text-white rounded-full text-2xl"></TfiWorld>
+          </p>
+          <h1 className="text-white">www.crigroups.com</h1>
         </div>
       </div>
     </section>
